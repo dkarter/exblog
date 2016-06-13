3bc2c52dc6b8263506d5fc5e842436f64414d9c7
Dorian Karter <dkarter@gmail.com>
2016-06-12 23:54:02 -0500

    Create basic blog in Phoenix and Elixir

---
+++ web/router.ex
@@ -0,0 +1,30 @@
+defmodule Exblog.Router do
+  use Exblog.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", Exblog do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+
+    resources "/posts", PostController do
+      post "/comment", PostController, :add_comment
+    end
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", Exblog do
+  #   pipe_through :api
+  # end
+end
